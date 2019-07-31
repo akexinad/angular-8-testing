6456261e9c76a1c220669b5f23753205de7acb7b
danny <danny.ceccattini@lendlease.com>
2019-07-31 15:16:51 +1000

    8. Structuring Angular Unit Tests - Test Setup using beforeEach

--- angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
+++ angular-testing-course-1-start/src/app/courses/services/calculator.service.spec.ts
@@ -3,26 +3,37 @@ import { LoggerService } from './logger.service';
 
 describe( 'calculator service', () => {
 
-    it('should add two numbers', () => {
+    // Define calculator up hear so it is thus available in every block.
+    let calculator: CalculatorService;
+    let loggerSpy: any = null;
 
+    // To avoid repeating yourself and creating the same instance of
+    // the logger sevice in every test, we can run the beforeEach() function.
+
+    beforeEach( () => {
+
+        // JASMINE SPY
         // Arg1 = the name of the service
         // Arg2 = the list of methods found on that service.
-        const logger = jasmine.createSpyObj('LoggerService', ['log']);
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
         // If using the above, there is no need to use the spyOn method.
         // spyOn(logger, 'log');
 
-        const calculator = new CalculatorService(logger);
+        calculator = new CalculatorService(loggerSpy);
+
+    });
+
+    it('should add two numbers', () => {
 
         const result = calculator.add(2, 2);
 
         expect(result).toBe(4);
 
-        expect(logger.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+
     });
 
     it('should subtract two numbers', () => {
-
-        const calculator = new CalculatorService( new LoggerService() );
 
         const result = calculator.subtract(2, 2);
 
@@ -32,6 +43,8 @@ describe( 'calculator service', () => {
         // to explain what is going on.
 
         // expect(result).toBe(0, 'unexpected subtraction result');
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
     });
 
